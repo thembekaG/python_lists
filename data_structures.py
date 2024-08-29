79fbc67462176ad20dddcb182139bedde9cf3d28
Thembeka Gumede <148956212+thembekaG@users.noreply.github.com>
2024-08-29 21:28:39 +0200

    Add files via upload

---
+++ data_structures.py
@@ -0,0 +1,32 @@
+# Empty string
+my_list = []
+
+# append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# inserting
+my_list.insert(1, 15)
+
+# extending
+my_list.extend([50, 60, 70])
+
+# Remove the last element from the list
+my_list.pop()
+
+# sorting in ascending order
+my_list.sort()
+
+# finding index
+print(my_list.index(30))
+
+
+
+
+
+
+
+
+
